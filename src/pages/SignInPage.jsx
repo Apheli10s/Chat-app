c1f528d1042cfae17cf494121b5e3573547beca6
grafika10 <grafika10@ucione.local>
2023-03-21 19:32:29 +0100

    21/03/2023

--- src/pages/SignInPage.jsx
+++ src/pages/SignInPage.jsx
@@ -2,10 +2,11 @@ import { useContext } from "react";
 import { SignInForm } from "../components/SignInForm";
 import { AppContext } from "../contexts/AppContext";
 import { Navigate } from "react-router-dom";
+import { MessageOfTheDay } from "../components/MessageOfTheDay";
 
 export function SignInPage(){
     const context = useContext(AppContext);
-
+   
     function handleSubmit(formData){
         context.setUsername(formData.username);
         context.setAvatarIndex(formData.avatarIndex)
@@ -17,6 +18,7 @@ export function SignInPage(){
 
     return (
         <div className ="sign-in-page">
+            <MessageOfTheDay />
             <div className="card">
                 <SignInForm onSubmit={handleSubmit} />
             </div>
