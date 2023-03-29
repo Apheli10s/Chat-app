bc5befbbd6939bf959948559f81be92764af0d7d
grafika10 <grafika10@ucione.local>
2023-03-29 19:07:43 +0200

    Redux example

--- src/App.js
+++ src/App.js
@@ -5,6 +5,30 @@ import { FaqPage } from "./pages/FaqPage.jsx";
 import { useContext } from "react";
 import { AppContext } from "./contexts/AppContext.js"
 
+const initialState = {  //Redux ˘ 
+  counter:0,
+  username: 'Mario'
+};
+
+function reducer(initialState, action){
+  console.log('Reducer', initialState, action); //ispise u konzoli, 'Reducer -> pocetno stanje -> akcija 
+
+  if(action.type === 'INCREMENT'){
+    return {...initialState, counter: initialState.counter + action.value}; // ...initSt kopira sve sto se nalazi u initial stateu i dodatno izvrsi akciju 
+  }                                                                           // bez ...initialState, gubimo username 
+  if(action.type === 'DECREMENT'){
+    return {...initialState, counter: initialState.counter - action.value};
+  }
+  return initialState;
+}
+
+const state1 = reducer(initialState, {type: 'INCREMENT', value: 10}); //type i value su dio objekta koji je 'action' parametar u funkciji
+const state2 = reducer(state1, {type: 'DECREMENT', value: 6});  
+
+
+console.log('REDUCER-1', state1);
+console.log('REDUCER-2', state2); //Redux ^
+
 function App() {
   const context = useContext(AppContext);
 
